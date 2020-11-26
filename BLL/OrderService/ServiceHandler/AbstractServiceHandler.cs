533e80a300fb1d4210b8c86dfa8eed9e70d3a96a
Василий Егоров <polevoy38@mail.ru>
2020-11-26 14:10:12 +0300

    решение

--- BLL/OrderService/ServiceHandler/AbstractServiceHandler.cs
+++ BLL/OrderService/ServiceHandler/AbstractServiceHandler.cs
@@ -6,16 +6,7 @@ namespace BLL.OrderService
 {
     public abstract class AbstractServiceHandler
     {
-        OrderDbContext orderDbContext;
         public abstract System_type System_Type { get; }
-        public OrderDbContext OrderDbContext
-        {
-            get {
-                return (orderDbContext is null)
-                    ? new OrderDbContext()
-                    : orderDbContext;
-            }
-        }
         public abstract string HandleOrder(string convertered_order);
     }
 }
